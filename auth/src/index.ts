1fdaa2dcb22478e4bb460acc2919759e3659d02d
Delwin Best <mail@delwinbest.com>
2021-07-04 12:18:06 +0900

    error handler and health check

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { healthzRouter } from './routes/healthz';
+
+import { errorHandler } from './middlewares/error-handler';
+
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  console.log(req.body);
-  res.send('Hi there...');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+app.use(healthzRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
