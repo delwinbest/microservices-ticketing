c3734e9bf5c0676643bf58a89bb25200246cb061
Delwin Best <mail@delwinbest.com>
2021-07-12 10:08:22 +0900

    just use axios. it works

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,28 +5,10 @@ const LandingPage = (props) => {
   return <h1>Default landing page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  let responseData = {};
-  let baseUrl = '';
-  // Check if we are rendering server side or in browser
-  if (typeof window === 'undefined') {
-    // Server Side
-    baseUrl = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
-  } else {
-    // Browser
-  }
-  try {
-    const response = await fetch(baseUrl + '/api/users/currentuser', {
-      headers: req.headers,
-    });
-    if (!response.ok) {
-      throw response;
-    }
-    responseData = await response.json();
-  } catch (err) {
-    console.log(err);
-  }
-  return responseData;
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
