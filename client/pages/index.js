9938bc2dd51f2fab2a87d1f4c488733c741e0954
Delwin Best <mail@delwinbest.com>
2021-07-12 10:03:48 +0900

    using fetch to pass headers and query  cluster

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,32 @@
-const index = () => {
+import buildClient from '../api/build-client';
+
+const LandingPage = (props) => {
+  console.log(props);
   return <h1>Default landing page</h1>;
 };
 
-export default index;
+LandingPage.getInitialProps = async ({ req }) => {
+  let responseData = {};
+  let baseUrl = '';
+  // Check if we are rendering server side or in browser
+  if (typeof window === 'undefined') {
+    // Server Side
+    baseUrl = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local';
+  } else {
+    // Browser
+  }
+  try {
+    const response = await fetch(baseUrl + '/api/users/currentuser', {
+      headers: req.headers,
+    });
+    if (!response.ok) {
+      throw response;
+    }
+    responseData = await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+  return responseData;
+};
+
+export default LandingPage;
