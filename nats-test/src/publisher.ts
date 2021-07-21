a4cb7458009d517ac3a01ffdf903f3102063e18c
Delwin Best <mail@delwinbest.com>
2021-07-21 13:37:16 +0900

    move publisher to its own class.

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 
 console.clear();
 const stan = nats.connect('ticketing', 'abc', {
@@ -8,17 +9,24 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
   console.log('Publisher connected to NATS');
 
-  const data = JSON.stringify({
-    id: '123',
-    title: 'Title',
-    price: 20,
+  const publisher = new TicketCreatedPublisher(stan);
+  publisher.publish({
+    id: '456',
+    title: 'Concert',
+    price: 33,
   });
 
-  stan.publish('ticket:created', data, (err, guid) => {
-    if (err) {
-      console.log('publish failed: ' + err);
-    } else {
-      console.log('published message with guid: ' + guid);
-    }
-  });
+  // const data = JSON.stringify({
+  //   id: '123',
+  //   title: 'Title',
+  //   price: 20,
+  // });
+
+  // stan.publish('ticket:created', data, (err, guid) => {
+  //   if (err) {
+  //     console.log('publish failed: ' + err);
+  //   } else {
+  //     console.log('published message with guid: ' + guid);
+  //   }
+  // });
 });
