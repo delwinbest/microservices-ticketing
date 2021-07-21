24b5450c4e205845b4e08f573962f0e85fc5b506
Delwin Best <mail@delwinbest.com>
2021-07-21 15:38:03 +0900

    add getter

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,7 @@ import { requireAuth, validateRequest } from '@drbtickets/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/tickets';
 import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -17,7 +18,7 @@ router.post(
     const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
     await ticket.save();
 
-    new TicketCreatedPublisher(client).publish({
+    new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
       price: ticket.price,
