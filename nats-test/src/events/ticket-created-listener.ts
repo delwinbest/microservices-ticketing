38686e3d6421582e242bff8bc10d4f66cf3b00b5
Delwin Best <mail@delwinbest.com>
2021-07-21 13:00:08 +0900

    typescript definitions for listener class

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,14 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './ticket-created-event';
 
-class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
-  queueGroupName = 'payents-service';
-  onMessage(data: any, msg: Message) {
+class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName = 'payments-service';
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event Data:', data);
+    console.log(data.title);
     msg.ack();
   }
 }
