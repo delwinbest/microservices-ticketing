3d6f770ab1f4a0fd3489ff3da593ef99c9fe2b37
Delwin Best <mail@delwinbest.com>
2021-07-22 10:25:23 +0900

    [orders] boiler plate microservice

---
+++ orders/src/routes/healthz.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/api/orders/healthz', (req, res) => {
+  res.status(200).send({});
+});
+
+export { router as healthzRouter };
