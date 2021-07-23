ff76e73e45cbd797a07a07d682c34e3c1ed23e8c
Delwin Best <mail@delwinbest.com>
2021-07-23 11:45:15 +0900

    create order and related ticket model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@drbtickets/common';
+import { TicketDoc } from './ticket';
+
+// An interface that descibes the new order properties
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// Interface that describes the properties that a order DOCUMENT has
+// (mongoose DB document has additional properties)
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+//Interface the describes properties that Order Model has
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.created,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+      required: false,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ticket',
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  },
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
