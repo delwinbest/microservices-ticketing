f095cb3239bd6cb30c7dbc0137512fd8073a91b8
Delwin Best <mail@delwinbest.com>
2021-07-24 15:48:52 +0900

    get and delete routes with tests

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+import { signin } from '../../test/signin';
+import mongoose from 'mongoose';
+
+it('order only be accessed if the user is signed in', async () => {
+  const fakeId = mongoose.Types.ObjectId().toHexString();
+  await request(app).delete(`/api/orders/${fakeId}`).send().expect(401);
+});
+
+it('returns an error if a user tries to acces another user order', async () => {
+  // Create ticket
+  const ticket = Ticket.build({ title: 'Ticket', price: 20 });
+  await ticket.save();
+  // Make request to build order with ticket
+  const user01 = signin();
+  const user02 = signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user01)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  //Make request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user02)
+    .expect(401);
+});
+
+it('marks an order as cancelled', async () => {
+  // Create a ticket
+  // Create ticket
+  const ticket = Ticket.build({ title: 'Ticket', price: 20 });
+  await ticket.save();
+  // Make request to build order with ticket
+  const user = signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  // Cancel the Order
+  const response = await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+  // Validate order state
+  const updatedOrder = await Order.findById(order.id);
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+  console.log(updatedOrder);
+});
+
+it.todo('publishes an order cancelled event');
---
+++ orders/src/routes/__test__/show.test.ts
@@ -0,0 +1,42 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { signin } from '../../test/signin';
+
+it('fetches the order', async () => {
+  // Create ticket
+  const ticket = Ticket.build({ title: 'Ticket', price: 20 });
+  await ticket.save();
+  // Make request to build order with ticket
+  const user = signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  //Make request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .expect(200);
+  expect(fetchedOrder.id).toEqual(order.id);
+});
+
+it('returns an error if a user tries to acces another user order', async () => {
+  // Create ticket
+  const ticket = Ticket.build({ title: 'Ticket', price: 20 });
+  await ticket.save();
+  // Make request to build order with ticket
+  const user01 = signin();
+  const user02 = signin();
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user01)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+  //Make request to fetch the order
+  const { body: fetchedOrder } = await request(app)
+    .get(`/api/orders/${order.id}`)
+    .set('Cookie', user02)
+    .expect(401);
+});
