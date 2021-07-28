775c97a4a00608a17abe92bbeeaa835f8409b008
Delwin Best <mail@delwinbest.com>
2021-07-29 08:44:07 +0900

    ticket updated listener test

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,50 @@
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+import { natsWrapper } from '../../../nats-wrapper';
+import { Ticket } from '../../../models/ticket';
+import mongoose from 'mongoose';
+import { TicketUpdatedEvent } from '@drbtickets/common';
+import { Message } from 'node-nats-streaming';
+
+const setup = async () => {
+  // Create a listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+  // Create and safe a ticket into our ticket collection
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: 'Ticket Title',
+    price: 10,
+  });
+  await ticket.save();
+  // Create a fake data object
+  const data: TicketUpdatedEvent['data'] = {
+    id: ticket.id,
+    version: ticket.version + 1,
+    title: 'Updated Title',
+    price: 15,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+  // Create a fake msg object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+  // Return objects
+  return { listener, data, ticket, msg };
+};
+
+it('finds, updates and saves a ticket', async () => {
+  const { listener, data, ticket, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+  const updatedTicket = await Ticket.findById(ticket.id);
+  expect(updatedTicket!.title).toEqual(data.title);
+  expect(updatedTicket!.price).toEqual(data.price);
+  expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acks the message', async () => {
+  const { listener, data, ticket, msg } = await setup();
+
+  await listener.onMessage(data, msg);
+  expect(msg.ack).toHaveBeenCalled();
+});
