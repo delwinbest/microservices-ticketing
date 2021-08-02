43bf726803f2a7ed7a28b8ad4017d05d0167d2da
Delwin Best <mail@delwinbest.com>
2021-08-03 08:05:52 +0900

    boilerplate for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookiesession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@drbtickets/common';
+
+import { healthzRouter } from './routes/healthz';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookiesession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+app.use(healthzRouter);
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
