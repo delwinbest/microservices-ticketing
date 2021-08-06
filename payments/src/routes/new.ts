d19dc1932c4e7626e29eb0b6f0548f327c3b6905
Delwin Best <mail@delwinbest.com>
2021-08-06 17:04:05 +0900

    implement stripe payments service

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -9,6 +9,7 @@ import {
   validateRequest,
 } from '@drbtickets/common';
 import { Order } from '../models/order';
+import { stripe } from '../stripe';
 
 const router = express.Router();
 
@@ -32,6 +33,13 @@ router.post(
     if (order.status === OrderStatus.Cancelled) {
       throw new BadRequestError('Cannot pay for a cancelled order.');
     }
+
+    await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+
     res.send({ success: true });
   },
 );
